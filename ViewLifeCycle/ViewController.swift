415c20cff679e8be5870cf739f12a102e12f0fa5
maweefeng <maweefeng@gmail.com>
2020-12-02 19:13:52 +0800

    Initial Commit

---
+++ ViewLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewLifeCycle
+//
+//  Created by 尚软科技 on 2020/12/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
